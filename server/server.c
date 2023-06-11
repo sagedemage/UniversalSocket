10185b311ca6d9fb851e2da16786f178b3a71314
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-11 18:50:00 -0400

    organize server code

--- server/server.c
+++ server/server.c
@@ -9,21 +9,32 @@
 #include <stdbool.h>
 #define PORT 8080
 
+static struct sockaddr_in setup_address(struct sockaddr_in address) {
+	/* Setup Address */
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+	return address;
+}
+
+static char* read_buffer(int new_socket, int buffer[1024]) {
+	/* Read the buffer */
+	read(new_socket, buffer, 1024);
+	return (char*)buffer;
+}
+
 int main() {
 	int server_fd, new_socket;
 	struct sockaddr_in address;
 	int opt = 1;
 	socklen_t addrlen = sizeof(address);
-	int buffer[1024] = { 0 };
+	int buf[1024] = { 0 };
 	char* msg;
 
 	// Setup Address
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
+	address = setup_address(address);
 
-	// Creating socket file descriptor
-	// SOCKET
+	// Create socket
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (server_fd == -1) {
@@ -31,26 +42,26 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 
-	// Forcefully attaching socket
+	// Set the socket options
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
 		perror("setsockopt");
 		exit(EXIT_FAILURE);
 	}
 	
-	// BIND
+	// Bind address to the socket
 	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) == -1) {
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
 
-	// LISTEN
+	// Listen for connections
 	if (listen(server_fd, 3) == -1) {
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
 
 	while(true) {
-		// ACCEPT
+		// Accept for connections
 		new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
 
  		if (new_socket == -1) {
@@ -58,9 +69,12 @@ int main() {
         	exit(EXIT_FAILURE);
     	}
 
-		// Read buffer
-		read(new_socket, buffer, 1024);
-		msg = (char*)buffer;
+		// Read the buffer
+		//read(new_socket, buf, 1024);
+		//msg = (char*)buf;
+
+		msg = read_buffer(new_socket, buf);
+
 		printf("%s\n", msg);
 	}
 }
