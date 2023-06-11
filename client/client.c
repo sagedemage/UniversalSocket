3a04d7084a63123cd262d3554f6631f548bfa9ce
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-11 18:57:13 -0400

    organize the client code

--- client/client.c
+++ client/client.c
@@ -8,6 +8,14 @@
 #define PORT 8080
 #define SERVER_ADDRESS "127.0.0.1"
 
+static struct sockaddr_in setup_address() {
+	/* Setup Address */
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(PORT);
+	return server_address;
+}
+
 int main() {
 	int status, client_fd;
 	struct sockaddr_in server_address;
@@ -20,8 +28,7 @@ int main() {
 		return -1;
 	}
 
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(PORT);
+	server_address = setup_address();
 
 	// Convert IPv4 and IPv6 addresses from text to binary form
 	if (inet_pton(AF_INET, SERVER_ADDRESS, &server_address.sin_addr) <= 0) {
@@ -29,16 +36,20 @@ int main() {
 		return -1;
 	}
 
+	// Open connection to socket
 	status = connect(client_fd, (struct sockaddr*)&server_address, sizeof(server_address));
 
 	if (status == -1) {
 		printf("Connection Failed \n");
 		return -1;
 	}
+
+	// Send a message to socket
 	send(client_fd, msg, strlen(msg), 0);
 	printf("Sent a message\n");
 
-	// closing the connected socket
+	// close the connected socket
 	close(client_fd);
+
 	return 0;
 }
